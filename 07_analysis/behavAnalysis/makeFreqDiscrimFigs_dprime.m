1a072a4923a109f4aabed4da853df7955d4a537e
pmalone333 <pmalone333@gmail.com>
2016-01-11 09:56:17 -0500

    adding new freq discrim task based on Josh/Clara's poster

--- 07_analysis/behavAnalysis/makeFreqDiscrimFigs_dprime.m
+++ 07_analysis/behavAnalysis/makeFreqDiscrimFigs_dprime.m
@@ -4,6 +4,7 @@ function makeFreqDiscrimFigs_dprime(sub,positionType,preOrPost,cfg)
     % passed args sub must be string (i.e '900' for subj 900), if
     % cfg isn't passed it is created using config_subjects_VT.m 
     % positionType = 1 for odd positions, 2 for even 
+    % preOrPost - 'preTrain' or 'postTrain'
     
     if exist('cfg','var') % if cfg was passed, use it; else, create one
         cfg = config_subjects_VT(cfg);
@@ -12,31 +13,33 @@ function makeFreqDiscrimFigs_dprime(sub,positionType,preOrPost,cfg)
     end
     
     % change preTrain to post for post training figs
-    data_path = dir(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim'));
-    load(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',data_path(3).name));
+    data_path = dir(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim','*block5.mat'));
+    if size(data_path,1)>1, error('More than 1 frequency discrimination data structure found'), end
+    load(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',data_path(1).name));
 
     %% d prime by block
     numBlocks = length(trialOutput);
-    dPrime = ones(numBlocks,1);
+    dPrimeByBlock = ones(numBlocks,1);
     for iBlock=1:numBlocks
         h = sum(trialOutput(iBlock).sResp(1:192) == 2 & trialOutput(iBlock).correctResponse(1:192) == 2)/sum(trialOutput(iBlock).correctResponse(1:192) == 2);
         fa = sum(trialOutput(iBlock).sResp(1:192) == 2 & trialOutput(iBlock).correctResponse(1:192) == 1)/sum(trialOutput(iBlock).correctResponse(1:192) == 1);
         if fa==0, fa=(1/sum(trialOutput(iBlock).sResp(1:192) == 2)); end
-        dPrime(iBlock) = calcDprime(h,fa);
+        dPrimeByBlock(iBlock) = calcDprime(h,fa);
         clear h fa
     end
-    plot(dPrime);
+    plot(dPrimeByBlock);
     xlabel('Block');
     ylabel('D prime');
     ylim([0 3.25])
     title(['Sub ' sub ' frequency discrim d prime by block']);
+    save(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',['sub' sub '_freqDiscrim_' preOrPost '_dPrime']),'dPrimeByBlock');
     print(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',['sub' sub '_freqDiscrim_' preOrPost '_dPrimeByBlock']),'-dpng');
     
     %% d prime by position
     if positionType==1, pos=[2 4 12 14]; 
     else pos=[1 3 11 13];
     end
-    dPrime = ones(length(pos),1);
+    dPrimeByPos = ones(length(pos),1);
     for p=1:length(pos)
         h = []; % hits
         fa = []; % false alarms
@@ -46,19 +49,20 @@ function makeFreqDiscrimFigs_dprime(sub,positionType,preOrPost,cfg)
             fa = [fa sum(trialOutput(iBlock).stimuli(2,1:192) == pos(p) & trialOutput(iBlock).sResp(1:192) == 2 & trialOutput(iBlock).correctResponse(1:192) == 1)/ ... 
                      sum(trialOutput(iBlock).stimuli(2,1:192) == pos(p) & trialOutput(iBlock).correctResponse(1:192) == 1)]; 
         end
-        dPrime(p) = calcDprime(h,fa);
+        dPrimeByPos(p) = calcDprime(h,fa);
     end
-    bar(dPrime);
+    bar(dPrimeByPos);
     xlabel('Position');
     ylabel('D prime');
     ylim([0 3.25])
     title(['Sub ' sub ' freq discrim d prime by position']);
     set(gca,'XTickLabel',pos)
+    save(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',['sub' sub '_freqDiscrim_' preOrPost '_dPrime']),'dPrimeByPos','-append');
     print(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',['sub' sub '_freqDiscrim_' preOrPost '_dPrimeByPos']),'-dpng');
        
     %% diff freq discrim d prime 
     freq = [26 40 61 93];
-    dPrime = ones(length(freq),1);
+    dPrimeByFreq = ones(length(freq),1);
     for f=1:length(freq)
         h = []; % hits
         fa = []; % false alarms
@@ -68,14 +72,15 @@ function makeFreqDiscrimFigs_dprime(sub,positionType,preOrPost,cfg)
            fa = [fa sum((floor(trialOutput(iBlock).stimuli(1,1:192)) == freq(f) | floor(trialOutput(iBlock).stimuli(3,1:192)) == freq(f)) & trialOutput(iBlock).sResp(1:192) == 2 & trialOutput(iBlock).correctResponse(1:192) == 1) / ... 
                     sum((floor(trialOutput(iBlock).stimuli(1,1:192)) == freq(f) | floor(trialOutput(iBlock).stimuli(3,1:192)) == freq(f)) & trialOutput(iBlock).correctResponse(1:192) == 1)]; 
         end
-        dPrime(f) = calcDprime(h,fa);
+        dPrimeByFreq(f) = calcDprime(h,fa);
     end
-    bar(dPrime)
+    bar(dPrimeByFreq)
     xlabel('Frequency');
     ylabel('D prime');
     ylim([0 3.25])
     title(['Sub ' sub ' freq discrim d prime by freq']);
     set(gca,'XTickLabel',freq)
+    save(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',['sub' sub '_freqDiscrim_' preOrPost '_dPrime']),'dPrimeByFreq','-append');
     print(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'freqDiscrim',['sub' sub '_freqDiscrim_' preOrPost '_dPrimeByFreq']),'-dpng');
     
 end
--- 07_analysis/behavAnalysis/makeSpatialLocFigs_dprime.m
+++ 07_analysis/behavAnalysis/makeSpatialLocFigs_dprime.m
@@ -12,34 +12,36 @@ function makeSpatialLocFigs_dprime(sub,positionType,preOrPost,cfg)
     end
     
     % change preTrain to post for post training figs
-    data_path = dir(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc'));
-    load(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',data_path(3).name));
+    data_path = dir(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc','*block7.mat'));
+    if size(data_path,1)>1, error('More than 1 spatial localization data structure found'), end
+    load(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',data_path(1).name));
 
     %% d prime by block
     numBlocks = length(trialOutput);
-    dPrime = ones(numBlocks,1);
-    for iBlock=2:numBlocks
+    dPrimeByBlock = ones(numBlocks,1);
+    for iBlock=1:numBlocks
         h = sum(trialOutput(iBlock).sResp(1:144) == 2 & trialOutput(iBlock).correctResponse(1:144) == 2)/sum(trialOutput(iBlock).correctResponse(1:144) == 2);
         fa = sum(trialOutput(iBlock).sResp(1:144) == 2 & trialOutput(iBlock).correctResponse(1:144) == 1)/sum(trialOutput(iBlock).correctResponse(1:144) == 1);
-        dPrime(iBlock) = calcDprime(h,fa);
+        dPrimeByBlock(iBlock) = calcDprime(h,fa);
         clear h fa
     end
-    plot(dPrime);
+    plot(dPrimeByBlock);
     xlabel('Block');
     ylabel('D prime');
     ylim([0 3.25])
     title(['Sub ' sub ' spatial localization d prime by block']);
+    save(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',['sub' sub '_spatialLoc_' preOrPost '_dPrime']),'dPrimeByBlock');
     print(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',['sub' sub '_spatialLoc_' preOrPost '_dPrimeByBlock']),'-dpng');
     
     %% d prime by position 
-    if positionType==2, pos=[2 4 6 10 12 14]; 
+    if positionType==1, pos=[2 4 6 10 12 14]; 
     else pos=[1 3 5 9 11 13];
     end
-    dPrime = ones(length(pos),1);
+    dPrimeByPos = ones(length(pos),1);
     for p=1:length(pos)
         h = []; % hits
         fa = []; % false alarms
-        for iBlock=2:numBlocks
+        for iBlock=1:numBlocks
            h = [h sum((floor(trialOutput(iBlock).stimuli(1,1:144)) == pos(p) | floor(trialOutput(iBlock).stimuli(3,1:144)) == pos(p)) & trialOutput(iBlock).sResp(1:144) == 2 & trialOutput(iBlock).correctResponse(1:144) == 2) / ... 
                    sum((floor(trialOutput(iBlock).stimuli(1,1:144)) == pos(p) | floor(trialOutput(iBlock).stimuli(3,1:144)) == pos(p)) & trialOutput(iBlock).correctResponse(1:144) == 2)]; 
            fa = [fa sum((floor(trialOutput(iBlock).stimuli(1,1:144)) == pos(p) | floor(trialOutput(iBlock).stimuli(3,1:144)) == pos(p)) & trialOutput(iBlock).sResp(1:144) == 2 & trialOutput(iBlock).correctResponse(1:144) == 1) / ... 
@@ -47,36 +49,38 @@ function makeSpatialLocFigs_dprime(sub,positionType,preOrPost,cfg)
         end
         if fa==0, fa=(1/sum(trialOutput(iBlock).stimuli(1,1:144) == pos(p))); end
         %if h==1, h=1-(1/sum(trialOutput(iBlock).stimuli(2,1:96) == pos(p))); end
-        dPrime(p) = calcDprime(h,fa);
+        dPrimeByPos(p) = calcDprime(h,fa);
     end
-    bar(dPrime);
+    bar(dPrimeByPos);
     xlabel('Position');
     ylabel('D prime');
     ylim([0 3.25])
     title(['Sub ' sub ' spatial localization d prime by position']);
     set(gca,'XTickLabel',pos)
+    save(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',['sub' sub '_spatialLoc_' preOrPost '_dPrime']),'dPrimeByPos','-append');
     print(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',['sub' sub '_spatialLoc_' preOrPost '_dPrimeByPos']),'-dpng');
     
     %% d prime by freq
     freq = [26 93];
-    dPrime = ones(length(freq),1);
+    dPrimeByFreq = ones(length(freq),1);
     for f=1:length(freq)
         h = []; % hits
         fa = []; % false alarms
-        for iBlock=2:numBlocks
+        for iBlock=1:numBlocks
             h = [h sum(floor(trialOutput(iBlock).stimuli(2,1:144)) == freq(f) & trialOutput(iBlock).sResp(1:144) == 2 & trialOutput(iBlock).correctResponse(1:144) == 2) / ... 
                    sum(floor(trialOutput(iBlock).stimuli(2,1:144)) == freq(f) & trialOutput(iBlock).correctResponse(1:144) == 2)]; 
             fa = [fa sum(floor(trialOutput(iBlock).stimuli(2,1:144)) == freq(f) & trialOutput(iBlock).sResp(1:144) == 2 & trialOutput(iBlock).correctResponse(1:144) == 1)/ ... 
                      sum(floor(trialOutput(iBlock).stimuli(2,1:144)) == freq(f) & trialOutput(iBlock).correctResponse(1:144) == 1)]; 
         end
-        dPrime(f) = calcDprime(h,fa);
+        dPrimeByFreq(f) = calcDprime(h,fa);
     end
-    bar(dPrime);
+    bar(dPrimeByFreq);
     xlabel('Frequency');
     ylabel('D prime');
     ylim([0 3.25])
     title(['Sub ' sub ' spatial localization d prime by frequency']);
     set(gca,'XTickLabel',freq)
+    save(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',['sub' sub '_spatialLoc_' preOrPost '_dPrime']),'dPrimeByFreq','-append');
     print(fullfile(cfg.dirs.behav_dir,sub,preOrPost,'spatialLoc',['sub' sub '_spatialLoc_' preOrPost '_dPrimeByFreq']),'-dpng');
     
 end
