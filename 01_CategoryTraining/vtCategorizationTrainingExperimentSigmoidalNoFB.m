18640e38ec9e5b795f062aaeb61a285261e07077
VT Laptop <levan.bokeria@gmail.com>
2016-04-11 13:56:23 -0400

    categorization task with no feedback for sigmoidal curve

--- 01_CategoryTraining/vtCategorizationTrainingExperimentSigmoidalNoFB.m
+++ 01_CategoryTraining/vtCategorizationTrainingExperimentSigmoidalNoFB.m
@@ -21,23 +21,23 @@ function vtCategorizationTrainingExperimentSigmoidalNoFB(name, exptdesign)
     correct1=imread(exptdesign.correct1, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
     correct2=imread(exptdesign.correct2, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
 
-%     %feedback for incorrect trials
-%     fb1=imread(exptdesign.fb1, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
-%     fb2=imread(exptdesign.fb2, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
-%     fb3=imread(exptdesign.fb3, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
-%     fb4=imread(exptdesign.fb4, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
+    %feedback for incorrect trials
+    fb1=imread(exptdesign.fb1, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
+    fb2=imread(exptdesign.fb2, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
+    fb3=imread(exptdesign.fb3, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
+    fb4=imread(exptdesign.fb4, 'png', 'BackgroundColor', [0.5 0.5 0.5]);
 
     fixationTexture=Screen('MakeTexture', w, double(fixationImage));
     blankTexture=Screen('MakeTexture', w, double(blankImage));
     response1Texture=Screen('MakeTexture', w, double(response1));
     response2Texture=Screen('MakeTexture', w, double(response2));
 
-%     fb1Texture=Screen('MakeTexture', w, double(fb1));
-%     fb2Texture=Screen('MakeTexture', w, double(fb2));
-%     fb3Texture=Screen('MakeTexture', w, double(fb3));
-%     fb4Texture=Screen('MakeTexture', w, double(fb4));
-%     correct1Texture=Screen('MakeTexture', w, double(correct1));
-%     correct2Texture=Screen('MakeTexture', w, double(correct2));
+    fb1Texture=Screen('MakeTexture', w, double(fb1));
+    fb2Texture=Screen('MakeTexture', w, double(fb2));
+    fb3Texture=Screen('MakeTexture', w, double(fb3));
+    fb4Texture=Screen('MakeTexture', w, double(fb4));
+    correct1Texture=Screen('MakeTexture', w, double(correct1));
+    correct2Texture=Screen('MakeTexture', w, double(correct2));
     %Display experiment instructions
     drawAndCenterText(w, ['Please review instructions \n'...
         'Please click a mouse button to advance at each screen'],1)
@@ -47,15 +47,28 @@ function vtCategorizationTrainingExperimentSigmoidalNoFB(name, exptdesign)
         'by clicking the mouse (left or right) according to the correct label\n'...
         'presented on the screen.'  ],1)
     
-    drawAndCenterText(w, ['There will be no feedback given this time for incorrect and correct answers.\n'], 1)
+    drawAndCenterText(w, ['There will be no feedback for incorrect or correct answers this time.'], 1)
     %load training stimuli
     load('trainingStimuliSigmoidalNoFB.mat');
+    level=exptdesign.level;
 
     for iBlock=1:exptdesign.numSessions %how many blocks to run this training session
-        drawAndCenterText(w,['Click the mouse to continue'],1);
+        drawAndCenterText(w,['Training Block #' num2str(iBlock) '. Click the mouse to continue'],1);
+%         if level > 5 && (exist(['./history/' name ], 'dir') == 7)
+%             clear trainingStimuli;
+%             filename = dir(['./history/' name '/training*']);
+%             filename = filename(length(filename)).name;
+%             load(['./history/' name '/' filename]);
+%             %randomize the stimuli for this level
+%             order = randperm(size(trainingStimuli{:},2));
+%             stimuli = trainingStimuli{1}(:,order);
+%         elseif level > 5
+%             order = randperm(size(trainingStimuli{5},2));
+%             stimuli = trainingStimuli{5}(:,order);
+%         else
             order = randperm(size(trainingStimuli{1},2));
             stimuli = trainingStimuli{1}(:,order);
-    end
+%         end
 
         %lower and upper limit of fixation before stimulus is presented
         ll=.3;
@@ -111,6 +124,31 @@ function vtCategorizationTrainingExperimentSigmoidalNoFB(name, exptdesign)
                 accuracy=0;
             end
 
+            %replay stimulus, show correct label, whether correct or
+            %incorrect
+%             if accuracy==0
+%                 %these 200 ms pauses prevent the code from skipping steps on
+%                 %the Dell Windows XP machine
+%                 %what was the correct answer?
+%                 Screen('DrawTexture', w, errorTexture);
+%                 WaitSecs(.1);
+%                 [CorrectionVBLTimestamp CorrectionOnsetTime CorrectionFlipTimestamp CorrectionMissed]=Screen('Flip', w);
+%                 KbPressWait(1);
+%                 drawAndCenterText(w, 'If you would like to play the vibration again\n\n click the RIGHT mouse button', 1.5);
+%                 [var1, keycode, var2] = KbPressWait(1);
+%                 %tResp = getResponseMouse2();
+%                 while keycode(1) ~= 1
+%                     rtn=-1;
+%                     while rtn==-1
+%                         rtn=stimGenPTB('start');
+%                     end
+%                     drawAndCenterText(w, 'If you would like to play the vibration again\n\n click the RIGHT mouse button', 1.5);
+%                     [var1, keycode, var2] = KbPressWait(1);
+%                     %tResp = getResponseMouse2();
+%                 end
+%             end
+
+
             %record parameters for the trial
             trialOutput(iBlock).responseStartTime(iTrial)=responseStartTime;
             trialOutput(iBlock).responseFinishedTime(iTrial)=responseFinishedTime;
@@ -130,15 +168,35 @@ function vtCategorizationTrainingExperimentSigmoidalNoFB(name, exptdesign)
             trialOutput(iBlock).RespFlipTimestamp(iTrial)=RespFlipTimestamp;
             trialOutput(iBlock).RespMissed(iTrial)=RespMissed;
 
+%             if accuracy==0
+%                 %[CorrectionVBLTimestamp CorrectionOnsetTime CorrectionFlipTimestamp CorrectionMissed]
+%                 trialOutput(iBlock).CorrectionVBLTimestamp(iTrial)=CorrectionVBLTimestamp;
+%                 trialOutput(iBlock).CorrectionOnsetTime(iTrial)=CorrectionOnsetTime;
+%                 trialOutput(iBlock).CorrectionFlipTimestamp(iTrial)=CorrectionFlipTimestamp;
+%                 trialOutput(iBlock).CorrectionMissed(iTrial)=CorrectionMissed;
+%             else
+%                 trialOutput(iBlock).CorrectionVBLTimestamp(iTrial)=NaN;
+%                 trialOutput(iBlock).CorrectionOnsetTime(iTrial)=NaN;
+%                 trialOutput(iBlock).CorrectionFlipTimestamp(iTrial)=NaN;
+%                 trialOutput(iBlock).CorrectionMissed(iTrial)=NaN;
+%                 trialOutput(iBlock).FeedbackVBLTimestamp(iTrial)=NaN;
+%                 trialOutput(iBlock).FeedbackOnsetTime(iTrial)=NaN;
+%                 trialOutput(iBlock).FeedbackFlipTimestamp(iTrial)=NaN;
+%                 trialOutput(iBlock).FeedbackMissed(iTrial)=NaN;
+%             end
+
+            %tell subject how they did on last trial
             if iTrial==exptdesign.numTrialsPerSession && iBlock < exptdesign.numSessions
                 %calculate accuracy
                 accuracyForLevel=mean(trialOutput(iBlock).accuracy);
-                drawAndCenterText(w, ['Your have completed block ' num2str(iBlock) '%\n\n\n'...
+                drawAndCenterText(w, ['You have completed block # ' num2str(iBlock) '\n\n\n'...
                     'Click mouse to continue' ],1)
                 KbWait(1)
 
             elseif iTrial==exptdesign.numTrialsPerSession && iBlock == exptdesign.numSessions
-                drawAndCenterText(w, ['You have completed this session.  Thank you for your work!' ],1)
+                %calculate accuracy
+                accuracyForLevel=mean(trialOutput(iBlock).accuracy);
+                drawAndCenterText(w, ['You have completed this training session.  Thank you for your work!' ],1)
                 KbWait(1)
                 Screen('CloseAll')
             end
@@ -155,10 +213,46 @@ function vtCategorizationTrainingExperimentSigmoidalNoFB(name, exptdesign)
         trialOutput(iBlock).accuracyForLevel=accuracyForLevel;
         trialOutput(iBlock).level = level;
 
-        f1 = 2.^((0:.1:2)+log2(25));
+        %check if they pass level and increase level if they do;
+        %levelAccuracy = [repmat(.75, [1 3]) .70 .75 .775 .80 .825 .85 .875 .9 .925 .90];
+
+%         f1 = 2.^((0:.1:2)+log2(25));
+%         if (accuracyForLevel >= levelAccuracy(level)) && level < exptdesign.maxLevel-1;
+%             level = level + 1;
+%         else
+%             counter = 1;
+%             for i = 1:size(trialOutput(iBlock).accuracy,2)
+%                 if stimuli(1,i) == f1(8) || stimuli(1,i) == f1(14)
+%                     level13Acc(counter) = trialOutput(iBlock).accuracy(i);
+%                     counter = counter + 1;
+%                 end
+%             end
+%             if mean(level13Acc) >= levelAccuracy(level)
+%                 level = level + 1;
+%             end
+%         end
+% 
+%         if level > 5 && level ~= exptdesign.maxLevel
+%             %call function that generates a weighted training stimuli file
+%             makeWeightedTrainingStimuli2(trialOutput(iBlock).accuracy, trialOutput(iBlock).stimuli, name);
+%         end
 
         %save the session data in the data directory
         save(['./data/' exptdesign.subNumber '/' datestr(now, 'yyyymmdd_HHMM') '-' exptdesign.subName '_block' num2str(iBlock) '.mat'], 'trialOutput', 'exptdesign');
+
+        %save the history data (stimuli, last level passed
+        %history = [exptdesign.training.history];
+        %exptdesign.training.history = history;
+        %lastLevelPassed = level;
+        %save(['./history/SUBJ' exptdesign.subNumber 'training.mat']);
+% 
+%         if level == exptdesign.maxLevel
+%             accuracyForLevel = mean(trialOutput(iBlock).accuracy);
+%             drawAndCenterText(w, ['Great Job! You have completed training! \n\n\n'...
+%                 'Your final accuracy was ' num2str(round(accuracyForLevel.*100))])
+%             Screen('CloseAll')
+%             break
+%         end
 
     end %end of block
     ShowCursor;
--- 01_CategoryTraining/vtCategorizationTrainingSigmoidalNoFB.m
+++ 01_CategoryTraining/vtCategorizationTrainingSigmoidalNoFB.m
@@ -1,7 +1,6 @@
-%Wrapper For Categoziation Training for generating sigmoidal categorization
-%curve. No feedback, positions 4 and 10 included for testing generalization
-%April 11 2016
-%PSM pmalone333@gmail.com
+%Wrapper For Categoziation Training
+%June 15 2015
+%CAS cas243@georgetown.edu
 
 input('\nDid you pull from Git? Hit Enter when "Yes."\n');
 input('\nIs white noise playing? Hit Enter when "Yes."\n');
@@ -25,6 +24,18 @@ else
     mkdir(['./data/' number])
 end
 
+%check if the subject has a saved level matrix.  If not, make it with
+%starting levels.
+%LOAD SUBJECT'S FILE TO APPEND
+% if exist(['./history/SUBJ' number 'training.mat'], 'file')
+%     exptdesign.training=load(['./history/SUBJ' number 'training.mat']);
+%     fprintf(['Subject has been here before, starting at level ' num2str(exptdesign.training.lastLevelPassed)])
+% else
+%     level=1;
+%     fprintf('Subject has not been entered before, starting at level 1')
+%     exptdesign.training.lastLevelPassed=1; %changed this to one cas 08/10/15
+%     exptdesign.training.history=[];
+% end
 pause(2)
 
 % exptdesign.level=exptdesign.training.lastLevelPassed;
@@ -36,9 +47,22 @@ exptdesign.numTrialsPerSession = 144;    % number of trials per block for level 
 
 exptdesign.fixationImage = 'imgsscaled/fixation.bmp';  % image for the fixation cross
 exptdesign.blankImage = 'imgsscaled/blank.bmp';        % image for the blank screen
+exptdesign.maxLevel = 14;
 
+exptdesign.giveFeedback=1;
 exptdesign.cat1label='imgsscaled/labelsGarkTrelp.png'; 
 exptdesign.cat2label='imgsscaled/labelsTrelpGark.png'; 
+
+%feedback for incorrect answers == 4 possibilities
+%edit 6/22/15 -- there will be no feedback for correct answers (advance
+%immediatley to next trial)
+exptdesign.fb1='imgsscaled/labelsWordSet1_wrong1.png';
+exptdesign.fb2='imgsscaled/labelsWordSet1_wrong2.png';
+exptdesign.fb3='imgsscaled/labelsWordSet2_wrong1.png';
+exptdesign.fb4='imgsscaled/labelsWordSet2_wrong2.png';
+%show correct answers
+exptdesign.correct1='imgsscaled/replayBlue.png';
+exptdesign.correct2='imgsscaled/replayRed.png';
 
 exptdesign.imageDirectory = 'imgsscaled/';
 
