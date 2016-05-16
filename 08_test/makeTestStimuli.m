cc67780d57699c7cf0fd9ad85ee994abc5cd1d6a
cs1471 <cs1471@georgetown.edu>
2016-05-16 12:31:04 -0400

    new positions for calib file

--- 08_test/makeTestStimuli.m
+++ 08_test/makeTestStimuli.m
@@ -4,7 +4,7 @@ f1=2.^((0:.1:2)+log2(25));
 frequency = f1(20);
 
 %generate positions based on response profile
-channels = [3 5 7 9];
+channels = [7 9 11 13];
     
 %generate frequencies to be compared with all possible positions
 stimuli = [repmat(frequency,1,4); channels];     
